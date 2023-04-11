c70316a4f3a138bb3428e3c5ec1d38e535436743
NicolaDiQuinzio <nicola.di.quinzio1@gmail.com>
2023-04-11 15:58:47 +0200

    final commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,39 @@
+/*
+Descrizione:
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+*/
+
 const { createApp } = Vue;
 
 createApp({
   data() {
-    return {};
+    return {
+      randomEmail: [],
+      emailApi: "https://flynn.boolean.careers/exercises/api/random/mail",
+    };
   },
-  methods: {},
-  mounted() {},
+  methods: {
+    getData() {
+      this.randomEmail = [];
+      for (let index = 0; index < 10; index++) {
+        axios
+          .get(this.emailApi)
+          .then((response) => {
+            this.randomEmail.push(response.data.response);
+          })
+          //Uso la funzione catch per gestire eventuali errori che possono verificarsi durante la richiesta HTTP
+          .catch((error) => {
+            console.error(error);
+          });
+      }
+    },
+  },
+  mounted() {
+    this.getData();
+  },
 }).mount("#app");
